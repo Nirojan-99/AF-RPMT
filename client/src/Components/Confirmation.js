f82616da819d7ad65ed2456f5e5aa70fb3c2009b
Nirojan21 <nirojan.yoga@gmail.com>
2022-05-15 14:29:38 +0530

    add popup

---
+++ client/src/Components/Confirmation.js
@@ -0,0 +1,38 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Slide,
+  } from "@mui/material";
+  import * as React from "react";
+  
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+  function Confirmation(props) {
+    return (
+      <div>
+        <Dialog
+          open={props.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={props.handleClose}
+        >
+          <DialogTitle>{"Remove member?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Do you Want to Remove</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={props.handleClose}>Yes</Button>
+            <Button onClick={props.handleClose}>No</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+  
+  export default Confirmation;
+  
