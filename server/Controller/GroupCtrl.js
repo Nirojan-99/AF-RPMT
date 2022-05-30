b097f14565ff35731a72feb8d5494beb764d2dd4
Lavaniyah.L <87692186+Lavaniyah31@users.noreply.github.com>
2022-05-30 18:57:32 +0530

    Add Submit tophic doc

--- server/Controller/GroupCtrl.js
+++ server/Controller/GroupCtrl.js
@@ -29,4 +29,52 @@ exports.GetStatus = (req, res) => {
         return res.status(404).json({ fetched: false });
       });
   };
+
+  //submit topic doc
+exports.AddTopicDoc = (req, res) => {
+    const { _id } = req.params;
+    const date = Date.now();
+  
+    if (req.files) {
+      let fileToUpload = req.files.doc;
+      const fileName = "topicdoc" + _id + date + fileToUpload.name;
+  
+      GroupModel.findById({ _id }, { research_Topic_doc: 1 })
+        .then((data) => {
+          if (data.research_Topic_doc) {
+            const path = data.research_Topic_doc.split(process.env.URL)[1];
+            fs.unlink(path, (er) => {
+              if (er) {
+                console.log(er);
+              } else {
+              }
+            });
+          }
+          console.log("here");
+          //add new
+          fileToUpload.mv("Uploads/" + fileName, (error) => {
+            if (error) {
+              console.log(error);
+              return res.status(404).json({ updated: false });
+            } else {
+              const link = `${process.env.URL}Uploads/` + fileName;
+              GroupModel.findByIdAndUpdate(
+                { _id },
+                { $set: { research_Topic_doc: link } }
+              )
+                .then((data) => {
+                  return res.status(200).json({ updated: true });
+                })
+                .catch((er) => {
+                  return res.status(404).json({ updated: false });
+                });
+            }
+          });
+        })
+        .catch((er) => {});
+    } else {
+      return res.status(404).json({ updated: false });
+    }
+  };
+  
   
